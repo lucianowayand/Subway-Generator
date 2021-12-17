2dddf77429a55f87663a2099102c4102f72c0fb0
Luciano <lucianowayand@hotmail.com>
2021-12-17 20:34:49 -0300

    create navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="navbar">
+        <img src="https://logos-download.com/wp-content/uploads/2016/03/Subway_logo_white.png" alt="Subway" style="width:6rem;">
+
+    </div>
+    
+</template>
+
+<style>
+    .navbar{
+        background-color: #145c40;
+        padding: 2rem;
+    }
+    .nav-content{
+        align-content: space-between;
+    }
+</style>
