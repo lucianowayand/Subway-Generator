e2679a036724883a6f9dd9f8e0f9707f2d3642a6
Luciano <lucianowayand@hotmail.com>
2021-12-18 10:28:48 -0300

    Adicionado navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,8 +1,13 @@
 <template>
-    <div class="navbar">
-        <img src="https://logos-download.com/wp-content/uploads/2016/03/Subway_logo_white.png" alt="Subway" style="width:6rem;">
-
+<div class="navbar">
+    <div class="logo">
+        <router-link to="/"><img src="https://logos-download.com/wp-content/uploads/2016/03/Subway_logo_white.png" alt="Subway" style="width:6rem;"></router-link>
     </div>
+    <div class="buttons-bar">
+        <router-link to="/sobre"><div class="button">SOBRE</div></router-link>
+        <router-link to="/historico"><div class="button">HISTORICO</div></router-link>
+    </div>
+</div>
     
 </template>
 
@@ -10,8 +15,22 @@
     .navbar{
         background-color: #145c40;
         padding: 2rem;
+        width: -webkit-fill-available;
+        display: flex;
+        justify-content: space-between;
     }
-    .nav-content{
-        align-content: space-between;
+    .buttons-bar{
+        color: #145c40;
+        font-weight: 900;
+        display: inline-flex;
+    }
+    .button{
+        background-color: white;
+        border-radius: 5px;
+        padding: 0.3rem;
+        margin-right: 10px;
+    }
+    .button:hover{
+        background-color: darkgrey;
     }
 </style>
